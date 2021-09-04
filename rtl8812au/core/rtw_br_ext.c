f581e262b2cd9962b9af36cc4eeb4e109d4f38c7
Mauro Rossi <issor.oruam@gmail.com>
2021-09-04 22:02:54 +0200

    rtl8812au: remove ipx support for kernel 5.15 and later
    Changes due to kernel 5.15 commit
    6c9b408 "net: Remove net/ipx.h and uapi/linux/ipx.h header files"

--- rtl8812au/core/rtw_br_ext.c
+++ rtl8812au/core/rtw_br_ext.c
@@ -21,8 +21,11 @@
 
 #ifdef __KERNEL__
 	#include <linux/if_arp.h>
+	#include <linux/version.h>
 	#include <net/ip.h>
+#if (LINUX_VERSION_CODE <= KERNEL_VERSION(5, 14, 0))
 	#include <net/ipx.h>
+#endif
 	#include <linux/atalk.h>
 	#include <linux/udp.h>
 	#include <linux/if_pppox.h>
@@ -157,6 +160,7 @@ static __inline__ void __nat25_generate_ipv4_network_addr(unsigned char *network
 	memcpy(networkAddr + 7, (unsigned char *)ipAddr, 4);
 }
 
+#if (LINUX_VERSION_CODE <= KERNEL_VERSION(5, 14, 0))
 static __inline__ void __nat25_generate_ipx_network_addr_with_node(unsigned char *networkAddr,
 		unsigned int *ipxNetAddr, unsigned char *ipxNodeAddr)
 {
@@ -186,6 +190,7 @@ static __inline__ void __nat25_generate_apple_network_addr(unsigned char *networ
 	memcpy(networkAddr + 1, (unsigned char *)network, 2);
 	networkAddr[3] = *node;
 }
+#endif
 
 static __inline__ void __nat25_generate_pppoe_network_addr(unsigned char *networkAddr,
 		unsigned char *ac_mac, __be16 *sid)
@@ -857,6 +862,7 @@ int nat25_db_handle(_adapter *priv, struct sk_buff *skb, int method)
 		}
 	}
 
+#if (LINUX_VERSION_CODE <= KERNEL_VERSION(5, 14, 0))
 	/*---------------------------------------------------*/
 	/*         Handle IPX and Apple Talk frame          */
 	/*---------------------------------------------------*/
@@ -1077,6 +1083,7 @@ int nat25_db_handle(_adapter *priv, struct sk_buff *skb, int method)
 
 		return -1;
 	}
+#endif
 
 	/*---------------------------------------------------*/
 	/*                Handle PPPoE frame                */
