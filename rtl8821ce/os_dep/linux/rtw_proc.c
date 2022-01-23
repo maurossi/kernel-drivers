8e55aa8963818a7bdb959d9ac07e8284e66533cb
Mauro Rossi <issor.oruam@gmail.com>
2022-01-23 06:16:57 +0100

    rtl8821ce: fix build with kernel 5.17
    Changes due to commits:
    
    cead1855 ("exit: Rename complete_and_exit to kthread_complete_and_exit")
    359745d7 ("proc: remove PDE_DATA() completely")

--- rtl8821ce/os_dep/linux/rtw_proc.c
+++ rtl8821ce/os_dep/linux/rtw_proc.c
@@ -212,7 +212,11 @@ const int drv_proc_hdls_num = sizeof(drv_proc_hdls) / sizeof(struct rtw_proc_hdl
 static int rtw_drv_proc_open(struct inode *inode, struct file *file)
 {
 	/* struct net_device *dev = proc_get_parent_data(inode); */
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 17, 0)
+	ssize_t index = (ssize_t)pde_data(inode);
+#else
 	ssize_t index = (ssize_t)PDE_DATA(inode);
+#endif
 	const struct rtw_proc_hdl *hdl = drv_proc_hdls + index;
 	void *private = NULL;
 
@@ -234,7 +238,11 @@ static int rtw_drv_proc_open(struct inode *inode, struct file *file)
 
 static ssize_t rtw_drv_proc_write(struct file *file, const char __user *buffer, size_t count, loff_t *pos)
 {
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 17, 0)
+	ssize_t index = (ssize_t)pde_data(file_inode(file));
+#else
 	ssize_t index = (ssize_t)PDE_DATA(file_inode(file));
+#endif
 	const struct rtw_proc_hdl *hdl = drv_proc_hdls + index;
 	ssize_t (*write)(struct file *, const char __user *, size_t, loff_t *, void *) = hdl->write;
 
@@ -3891,7 +3899,11 @@ const int adapter_proc_hdls_num = sizeof(adapter_proc_hdls) / sizeof(struct rtw_
 
 static int rtw_adapter_proc_open(struct inode *inode, struct file *file)
 {
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 17, 0)
+	ssize_t index = (ssize_t)pde_data(inode);
+#else
 	ssize_t index = (ssize_t)PDE_DATA(inode);
+#endif
 	const struct rtw_proc_hdl *hdl = adapter_proc_hdls + index;
 	void *private = proc_get_parent_data(inode);
 
@@ -3913,7 +3925,11 @@ static int rtw_adapter_proc_open(struct inode *inode, struct file *file)
 
 static ssize_t rtw_adapter_proc_write(struct file *file, const char __user *buffer, size_t count, loff_t *pos)
 {
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 17, 0)
+	ssize_t index = (ssize_t)pde_data(file_inode(file));
+#else
 	ssize_t index = (ssize_t)PDE_DATA(file_inode(file));
+#endif
 	const struct rtw_proc_hdl *hdl = adapter_proc_hdls + index;
 	ssize_t (*write)(struct file *, const char __user *, size_t, loff_t *, void *) = hdl->write;
 
@@ -4078,7 +4094,11 @@ const int odm_proc_hdls_num = sizeof(odm_proc_hdls) / sizeof(struct rtw_proc_hdl
 
 static int rtw_odm_proc_open(struct inode *inode, struct file *file)
 {
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 17, 0)
+	ssize_t index = (ssize_t)pde_data(inode);
+#else
 	ssize_t index = (ssize_t)PDE_DATA(inode);
+#endif
 	const struct rtw_proc_hdl *hdl = odm_proc_hdls + index;
 	void *private = proc_get_parent_data(inode);
 
@@ -4100,7 +4120,11 @@ static int rtw_odm_proc_open(struct inode *inode, struct file *file)
 
 static ssize_t rtw_odm_proc_write(struct file *file, const char __user *buffer, size_t count, loff_t *pos)
 {
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 17, 0)
+	ssize_t index = (ssize_t)pde_data(file_inode(file));
+#else
 	ssize_t index = (ssize_t)PDE_DATA(file_inode(file));
+#endif
 	const struct rtw_proc_hdl *hdl = odm_proc_hdls + index;
 	ssize_t (*write)(struct file *, const char __user *, size_t, loff_t *, void *) = hdl->write;
 
@@ -4237,7 +4261,11 @@ const int mcc_proc_hdls_num = sizeof(mcc_proc_hdls) / sizeof(struct rtw_proc_hdl
 
 static int rtw_mcc_proc_open(struct inode *inode, struct file *file)
 {
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 17, 0)
+	ssize_t index = (ssize_t)pde_data(inode);
+#else
 	ssize_t index = (ssize_t)PDE_DATA(inode);
+#endif
 	const struct rtw_proc_hdl *hdl = mcc_proc_hdls + index;
 	void *private = proc_get_parent_data(inode);
 
@@ -4259,7 +4287,11 @@ static int rtw_mcc_proc_open(struct inode *inode, struct file *file)
 
 static ssize_t rtw_mcc_proc_write(struct file *file, const char __user *buffer, size_t count, loff_t *pos)
 {
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 17, 0)
+	ssize_t index = (ssize_t)pde_data(file_inode(file));
+#else
 	ssize_t index = (ssize_t)PDE_DATA(file_inode(file));
+#endif
 	const struct rtw_proc_hdl *hdl = mcc_proc_hdls + index;
 	ssize_t (*write)(struct file *, const char __user *, size_t, loff_t *, void *) = hdl->write;
 
