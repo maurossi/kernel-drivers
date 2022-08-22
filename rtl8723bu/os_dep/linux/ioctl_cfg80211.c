b80523fc6072026645b4c45cf5a72aa236997c06
Mauro Rossi <issor.oruam@gmail.com>
2022-08-23 00:06:02 +0200

    rtl8723bu: fix build with kernel 6.0
    Changes due to following commits:
    
    b327c84c328 "wifi: mac80211: replace link_id with link_conf in start/stop_ap()"
    efbabc11650 "cfg80211: Indicate MLO connection info in connect and roam callbacks"
    ae7ba17b49b "wifi: mac80211: pass the link id in start/stop ap"
    7b0a0e3c3a8 "wifi: cfg80211: do some rework towards MLO link APIs"
    
    Fixes the following building errors:
    
    /home/utente/q-x86/out/target/product/x86_64/obj/kmodule/rtl8723bu/os_dep/linux/ioctl_cfg80211.c:823:13: error: no member named 'bssid' in 'struct cfg80211_roam_info'
                    roam_info.bssid = cur_network->network.MacAddress;
                    ~~~~~~~~~ ^
    /home/utente/q-x86/out/target/product/x86_64/obj/kmodule/rtl8723bu/os_dep/linux/ioctl_cfg80211.c:6524:13: error: incompatible pointer types initializing 'int (*)(struct wiphy *, struct net_device *, unsigned int)' with an expression of type 'int (struct wiphy *, struct net_device *)' [-Werror,-Wincompatible-pointer-types]
            .stop_ap = cfg80211_rtw_stop_ap,
                       ^~~~~~~~~~~~~~~~~~~~
    /home/utente/q-x86/out/target/product/x86_64/obj/kmodule/rtl8723bu/os_dep/linux/ioctl_cfg80211.c:6713:12: error: no member named 'current_bss' in 'struct wireless_dev'
            if (wdev->current_bss) {
                ~~~~  ^
    3 errors generated.

--- rtl8723bu/os_dep/linux/ioctl_cfg80211.c
+++ rtl8723bu/os_dep/linux/ioctl_cfg80211.c
@@ -820,7 +820,11 @@ check_bss:
 
 		DBG_871X(FUNC_ADPT_FMT" call cfg80211_roamed\n", FUNC_ADPT_ARG(padapter));
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(4,12,0))
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(6,0,0))
+		roam_info.links[0].bssid = cur_network->network.MacAddress;
+#else
 		roam_info.bssid = cur_network->network.MacAddress;
+#endif /* (LINUX_VERSION_CODE >= KERNEL_VERSION(6,0,0)) */
 		roam_info.req_ie = pmlmepriv->assoc_req+sizeof(struct rtw_ieee80211_hdr_3addr)+2;
 		roam_info.req_ie_len = pmlmepriv->assoc_req_len-sizeof(struct rtw_ieee80211_hdr_3addr)-2;
 		roam_info.resp_ie = pmlmepriv->assoc_rsp+sizeof(struct rtw_ieee80211_hdr_3addr)+6,
@@ -4317,7 +4321,11 @@ static int cfg80211_rtw_change_beacon(struct wiphy *wiphy, struct net_device *nd
 	return ret;
 }
 
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(6,0,0))
+static int cfg80211_rtw_stop_ap(struct wiphy *wiphy, struct net_device *ndev, unsigned int link_id)
+#else
 static int cfg80211_rtw_stop_ap(struct wiphy *wiphy, struct net_device *ndev)
+#endif /* (LINUX_VERSION_CODE >= KERNEL_VERSION(6,0,0)) */
 {
 	DBG_871X(FUNC_NDEV_FMT"\n", FUNC_NDEV_ARG(ndev));
 	return 0;
@@ -6710,7 +6718,11 @@ void rtw_wdev_unregister(struct wireless_dev *wdev)
 	rtw_cfg80211_indicate_scan_done(adapter, _TRUE);
 
 	#if (LINUX_VERSION_CODE >= KERNEL_VERSION(4, 2, 0))
+	#if (LINUX_VERSION_CODE >= KERNEL_VERSION(6, 0, 0))
+	if (wdev->links[0].client.current_bss) {
+	#else
 	if (wdev->current_bss) {
+	#endif /* (LINUX_VERSION_CODE >= KERNEL_VERSION(6, 0, 0)) */
 		u8 locally_generated = 1;
 		DBG_871X(FUNC_ADPT_FMT" clear current_bss by cfg80211_disconnected\n", FUNC_ADPT_ARG(adapter));
 		cfg80211_disconnected(adapter->pnetdev, 0, NULL, 0, locally_generated, GFP_ATOMIC);
