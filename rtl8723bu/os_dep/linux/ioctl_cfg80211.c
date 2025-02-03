14abd257d8e09659089e2bf3a744b1c22110ac19
Mauro Rossi <issor.oruam@gmail.com>
2025-02-03 07:25:30 -0500

    rtl8723bu: fix build with kernel 6.14
    Changes due to kernel commit 24dab555 ("wifi: mac80211: get tx power per link")

--- rtl8723bu/os_dep/linux/ioctl_cfg80211.c
+++ rtl8723bu/os_dep/linux/ioctl_cfg80211.c
@@ -3509,6 +3509,9 @@ static int cfg80211_rtw_get_txpower(struct wiphy *wiphy,
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(3,8,0))
 	struct wireless_dev *wdev,
 #endif
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 14, 0)
+	unsigned int link_id,
+#endif
 	int *dbm)
 {
 	DBG_8192C("%s\n", __func__);
