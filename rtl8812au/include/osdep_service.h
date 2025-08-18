8942b8258fcbf9ee677601080d623331169e06f2
Mauro Rossi <issor.oruam@gmail.com>
2025-08-18 05:29:16 -0400

    rtl8812au: fix build with kernel 6.15
    Changes due to kernel commits:
    8fa7292 "treewide: Switch/rename to timer_delete[_sync]()"
    e966ad0 "kbuild: remove EXTRA_*FLAGS support"

--- rtl8812au/include/osdep_service.h
+++ rtl8812au/include/osdep_service.h
@@ -207,7 +207,11 @@ extern void rtw_init_timer(_timer *ptimer, void *padapter, void *pfunc);
 
 __inline static unsigned char _cancel_timer_ex(_timer *ptimer)
 {
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(6, 15, 0))
+	return timer_delete_sync(ptimer);
+#else
 	return del_timer_sync(ptimer);
+#endif
 }
 
 static __inline void thread_enter(char *name)
